c2476743c3fcf84b0d308e901fef02043a380b66
qiqing <maqiqing@me.com>
2022-06-23 13:57:38 +0800

    Initial Commit

---
+++ SwiftUIRegistration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIRegistration
+//
+//  Created by maqiqing on 2022/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIRegistration/SwiftUIRegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRegistrationApp.swift
+//  SwiftUIRegistration
+//
+//  Created by maqiqing on 2022/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
